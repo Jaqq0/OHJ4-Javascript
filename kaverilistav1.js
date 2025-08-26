d560abb132822986821310b8f902290d09a0680d
Jaqq0 <vehmas.timo@gmail.com>
2025-08-26 12:55:44 +0300

    kaverilista v1

---
+++ kaverilistav1.js
@@ -0,0 +1,14 @@
+const kaverit =[];
+for (let i= 0; i < 10; i++) {
+    let nimi = prompt(`Anna kaverin nimi (${i+1}/10):`);
+    while (!nimi) {
+        nimi = prompt(`Nimi ei voi olla tyhjä. Anna kaverin nimi (${i+1}/10):`);
+    }
+    kaverit.push(nimi);
+}
+const lista = document.getElementById("lista");
+kaverit.forEach(kaveri => {
+    const li = document.createElement("li");
+    li.textContent = kaveri;
+    lista.appendChild(li);
+});
