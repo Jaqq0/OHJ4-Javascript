68343a74556fdc2c0d86552946f27cff025988d4
Jaqq0 <vehmas.timo@gmail.com>
2025-08-28 14:11:31 +0300

    Lisätty aloitus nappi

--- kaverilistav1.js
+++ kaverilistav1.js
@@ -1,14 +1,17 @@
-const kaverit =[];
-for (let i= 0; i < 10; i++) {
-    let nimi = prompt(`Anna kaverin nimi (${i+1}/10):`);
-    while (!nimi) {
-        nimi = prompt(`Nimi ei voi olla tyhjä. Anna kaverin nimi (${i+1}/10):`);
+function teeLista() {
+    const kaverit =[];
+    for (let i= 0; i < 10; i++) {
+        let nimi = prompt(`Anna kaverin nimi (${i+1}/10):`);
+        while (!nimi) {
+            nimi = prompt(`Nimi ei voi olla tyhjä. Anna kaverin nimi (${i+1}/10):`);
+        }
+        kaverit.push(nimi);
     }
-    kaverit.push(nimi);
+    const lista = document.getElementById("lista");
+    lista.innerHTML = "";
+    kaverit.forEach(kaveri => {
+        const li = document.createElement("li");
+        li.textContent = kaveri;
+        lista.appendChild(li);
+    });
 }
-const lista = document.getElementById("lista");
-kaverit.forEach(kaveri => {
-    const li = document.createElement("li");
-    li.textContent = kaveri;
-    lista.appendChild(li);
-});
