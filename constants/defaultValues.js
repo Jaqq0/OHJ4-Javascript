5517a1145ac9c99a935864363a4b6e11f16218c0
Jaqq0 <vehmas.timo@gmail.com>
2025-10-17 11:01:23 +0300

    kuvine linkkejä

--- constants/defaultValues.js
+++ constants/defaultValues.js
@@ -1,7 +1,7 @@
 export const defaultValues = [
-    {name: 'bolt', image: './img/bolt.png', cost: 10, increase: 1},
-    {name: 'doublenut', image: './img/double nut.png', cost: 130, increase: 4},
-    {name: 'wrench', image: './img/wrench.png', cost: 850, increase: 24},
-    {name: 'gear', image: './img/gear.png', cost: 4750, increase: 82},
-    {name: 'bionicarm', image: './img/arm.png', cost: 23550, increase: 422},
+    {name: 'bolt', image: './Img/bolt.png', cost: 10, increase: 1},
+    {name: 'doublenut', image: './Img/double nut.png', cost: 130, increase: 4},
+    {name: 'wrench', image: './Img/wrench.png', cost: 850, increase: 24},
+    {name: 'gear', image: './Img/gear.png', cost: 4750, increase: 82},
+    {name: 'bionicarm', image: './Img/arm.png', cost: 23550, increase: 422},
 ]
